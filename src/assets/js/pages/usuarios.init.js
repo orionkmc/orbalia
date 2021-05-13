708221adf3133efa2a48cf8ef09e08efda334877
kuai-mare carrasquero <orionkmc@gmail.com>
2021-05-13 19:26:12 -0400

    plantilla perfil-sociedad

--- src/assets/js/pages/usuarios.init.js
+++ src/assets/js/pages/usuarios.init.js
@@ -18,4 +18,86 @@ $(document).ready(function () {
     info: false,
     searching: false,
   });
+  $("#datatable5").DataTable({
+    paging: false,
+    info: false,
+    searching: false,
+  });
+  //Buttons examples
+  var table = $("#datatable-buttons").DataTable({
+    paging: false,
+    info: false,
+    searching: false,
+    lengthChange: false,
+    buttons: ["colvis"],
+  });
+
+  table
+    .buttons()
+    .container()
+    .appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)");
+  //Buttons examples
+  var table = $("#datatable2-buttons").DataTable({
+    paging: false,
+    info: true,
+    searching: false,
+    lengthChange: false,
+    buttons: ["excel", "pdf", "colvis"],
+    language: {
+      buttons: {
+        excel: "Excel",
+        pdf: "PDF",
+        colvis: "Columnas Visibles",
+      },
+      info: "Mostrando _START_ de _END_ de _TOTAL_ entradas",
+    },
+  });
+
+  table
+    .buttons()
+    .container()
+    .appendTo("#datatable2-buttons_wrapper .col-md-6:eq(0)");
+
+  //Buttons examples
+  var table = $("#datatable3-buttons").DataTable({
+    paging: false,
+    info: true,
+    searching: false,
+    lengthChange: false,
+    buttons: ["excel", "pdf", "colvis"],
+    language: {
+      buttons: {
+        excel: "Excel",
+        pdf: "PDF",
+        colvis: "Columnas Visibles",
+      },
+      info: "Mostrando _START_ de _END_ de _TOTAL_ entradas",
+    },
+  });
+
+  table
+    .buttons()
+    .container()
+    .appendTo("#datatable3-buttons_wrapper .col-md-6:eq(0)");
+
+  //Buttons examples
+  var table = $("#datatable4-buttons").DataTable({
+    paging: false,
+    info: false,
+    searching: false,
+    lengthChange: false,
+    buttons: ["excel", "pdf", "colvis"],
+    language: {
+      buttons: {
+        excel: "Excel",
+        pdf: "PDF",
+        colvis: "Columnas Visibles",
+      },
+    },
+  });
+
+  table
+    .buttons()
+    .container()
+    .appendTo("#datatable4-buttons_wrapper .col-md-6:eq(0)");
 });
